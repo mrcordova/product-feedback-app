8d2484cc266376f1aa8b564781f67cefca762d66
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-02 13:44:00 -0700

    add comment element styling

--- js/details.js
+++ js/details.js
@@ -1,4 +1,8 @@
-console.log(localStorage.getItem("post"));
+// console.log(localStorage.getItem("post"));
+const dataResponse = await fetch("data.json");
+const data = await dataResponse.json();
+
+console.log(data);
 
 const postWrapper = document.querySelector("[data-post-wrapper]");
 postWrapper.insertAdjacentHTML("beforeend", localStorage.getItem("post"));
