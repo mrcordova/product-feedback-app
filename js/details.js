fddec1abbcbc7765f9a6c6e48f0d9f3ce394e807
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-02 09:29:01 -0700

    insert post into post wrapper

--- js/details.js
+++ js/details.js
@@ -1,1 +1,4 @@
 console.log(localStorage.getItem("post"));
+
+const postWrapper = document.querySelector("[data-post-wrapper]");
+postWrapper.insertAdjacentHTML("beforeend", localStorage.getItem("post"));
