30ac4268f29e2ab82b0600fba5bdcea9ee9a2d20
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-11-05 14:36:42 -0800

    logic for dropdown selection

--- js/edit.js
+++ js/edit.js
@@ -9,7 +9,7 @@ const feedbackDetail = document.querySelector("#detail");
 
 const post = getPost(localStorage.getItem("post_id"));
 
-console.log(post);
+// console.log(post);
 
 header.textContent = `Editing '${post.title}'`;
 feedbackTitle.value = post.title;
@@ -17,11 +17,31 @@ categoryDisplay.textContent = `${post.category}`;
 statusDisplay.textContent = post.status;
 feedbackDetail.value = post.description;
 
+function showDropdown(dropdown) {
+  const checkboxEle = dropdown.nextElementSibling;
+  checkboxEle.checked = !checkboxEle.checked;
+}
+
 main.addEventListener("click", (e) => {
-  //   e.preventDefault();
+  e.preventDefault();
   const goBackBtn = e.target.closest("[data-go-back]");
+  const choice = e.target.closest("[data-sort-by-choice]");
+  const dropdownCate = e.target.closest("[data-category]");
+  const dropdownStat = e.target.closest("[data-status]");
 
   if (goBackBtn) {
     goBack();
+  } else if (choice) {
+    // console.log(choice);
+    const inputChoice = choice.querySelector('input[type="radio"]');
+    const categoryDisplay = choice
+      .closest("menu")
+      .previousElementSibling.querySelector(".dropdown-cont");
+
+    inputChoice.checked = !inputChoice.checked;
+    categoryDisplay.textContent = choice.dataset.sortByChoice;
+    showDropdown(categoryDisplay);
+  } else if (dropdownCate || dropdownStat) {
+    showDropdown(dropdownCate ?? dropdownStat);
   }
 });
